5e05125c3306be156765b74cbb73e0dda7331707
Davin <65677163+davin2020@users.noreply.github.com>
2022-02-04 16:10:07 +0000

    added timestamp option and set userid to be of type ObjectId, for GoalModel

--- mongoModels/GoalModel.js
+++ mongoModels/GoalModel.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // ISSUE doesnt work if userid is mongoose.ObjectId, has to be String
+// re Mongoose Timestamps - When you enable timestamps, Mongoose adds createdAt and updatedAt properties to your schema. By default, createdAt and updatedAt are of type Date. When you update a document, Mongoose automatically increments updatedAt.
+//thsi works to save as object id!
 const goalSchema = new Schema({
-	userid: String,
+	userid: mongoose.Types.ObjectId,
     name: String,
     is_target_type: Boolean,
     target_amount_goal: Number,
@@ -12,9 +14,9 @@ const goalSchema = new Schema({
     points: Number,
     is_completed: Boolean,
     is_deleted: Boolean
-});
+},
+	{ timestamps: true }
+);
 
 //first param is name of relevant collection in MongoDB where data is saved
 module.exports = mongoose.model('goals', goalSchema);
-
-
